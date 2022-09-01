6d91db344ed1f9355bb3cec030eaf991378e2cd1
Tom Spencer <thomas.spencer@codurance.com>
2022-09-01 06:25:25 +0100

    Add code for static array

--- src/main/java/com/tomspencerlondon/StaticArray.java
+++ src/main/java/com/tomspencerlondon/StaticArray.java
@@ -5,11 +5,52 @@ import java.util.Arrays;
 public class StaticArray {
 
   public static void main(String[] args) {
-    int[] arr = new int[4];
+    int[] arr = new int[5];
     insertEnd(arr, 3, 3, 4);
     System.out.println(Arrays.toString(arr));
     removeEnd(arr, 4);
-    System.out.println(Arrays.toString(arr));
+    printArr(arr, 5);
+    insertMiddle(arr, 2, 5, 4);
+    printArr(arr, 5);
+    removeMiddle(arr, 1, 5);
+    printArr(arr, 5);
+  }
+
+  // Remove value at index i before shifting elements to the left.
+  // Assuming i is a valid index.
+  public static void removeMiddle(int[] arr, int i, int length) {
+    // Shift starting from i + 1 to the end.
+    for (int index = i + 1; index < length; index++) {
+      arr[index - 1] = arr[index];
+    }
+
+    // No need to 'remove' arr[i], since we already shifted
+  }
+
+  // insert n into index i after shifting elements to the right.
+  // Assuming i is a valid index and arr is not full.
+  public static void insertMiddle(int[] arr, int i, int n, int length) {
+    // Shift starting from the end to i.
+    for (int index = length - 1; index > i - 1; index--) {
+      arr[index + 1] = arr[index];
+    }
+    // insert at i
+    arr[i] = n;
+  }
+
+  public static void printArr(int[] arr, int capacity) {
+    System.out.print("[");
+    for (int i = 0; i < capacity; i++) {
+      if (i > 0) {
+        System.out.print(" ");
+      }
+      System.out.print(arr[i]);
+
+      if (i < capacity - 1) {
+        System.out.print(",");
+      }
+    }
+    System.out.println("]");
   }
 
   // Remove from the last position in the array of the array
